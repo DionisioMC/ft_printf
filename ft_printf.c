b7912e0b2e39b08bf71aae3b464e0f3ada40f6e7
Dionisio Coelho <dcoelho@c1r10s5.42porto.com>
2026-05-01 16:26:03 +0100

    printf

--- ft_printf.c
+++ ft_printf.c
@@ -6,11 +6,19 @@
 /*   By: dcoelho <dcoelho@student.42porto.com>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/04/29 16:58:15 by dcoelho           #+#    #+#             */
-/*   Updated: 2026/05/01 15:27:30 by dcoelho          ###   ########.fr       */
+/*   Updated: 2026/05/01 16:21:43 by dcoelho          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "ft_printf.h"
+
+void	interpret_flag(char c, va_list arg)
+{
+	if (c == 'c')
+	{
+		ft_putchar_fd(va_arg(arg, int), 1);
+	}
+}
 
 int	ft_printf(const char *format, ...)
 {
@@ -23,9 +31,14 @@ int	ft_printf(const char *format, ...)
 	va_start(args, format);
 	while(format[i])
 	{
-		if (format[i] == '%' && format[i + 1] == 'c')
+		if (format[i] == '%')
 		{
-			ft_putchar_fd('d', 1);
+			i++;
+			interpret_flag(format[i], args);
+		}
+		else
+		{
+			ft_putchar_fd(format[i], 1);
 		}
 		i++;
 	}
